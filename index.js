2a365dc6940516600ed7681207be9cade4631496
pwangy <blipsandclicks@gmail.com>
2024-01-11 13:15:20 -0800

    all tests pass

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+document.querySelector('main#main').remove()
+
+document.body.onload = addElement
+
+const newHeader = document.createElement('h1')
+
+function addElement() {
+    newHeader.id = 'victory'
+    newHeader.innerHTML = 'Peggy is the champion'
+
+    const currentNode = document.getElementById('main')
+    document.body.insertBefore(newHeader, currentNode)
+}
